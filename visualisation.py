7c3411927ac8e45cd64f3a4dce07f6df07984922
nw316 <nw316@ic.ac.uk>
2017-12-25 04:25:08 +0000

    added legend to visualisation

--- visualisation.py
+++ visualisation.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.datasets import fetch_mldata
 
-def get_color_from_class_index(class_index):
+def get_color_and_label(class_index):
     colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
-    return colors[class_index]
-    
+    class_labels = [ "T-shirt/top - 0", "Trouser - 1", "Pullover - 2",  "Dress - 3", "Coat - 4", "Sandal - 5", "Shirt - 6", "Sneaker - 7", "Bag - 8", "Ankle boot - 9"]
+    return colors[class_index], class_labels[class_index]
+
 from reader import get_images
 (X_train, y_train), _ = get_images() 
 
@@ -14,13 +15,25 @@ pca = PCA(n_components=2)
 X_transformed = pca.fit_transform(X_train)
 
 fig, plot = plt.subplots()
-fig.set_size_inches(50, 50)
+fig.set_size_inches(25, 25)
 
-color_array = [get_color_from_class_index(y_train[i]) for i in range(len(X_transformed))]
-plot.scatter(X_transformed[:, 0], X_transformed[:, 1], c=color_array)
+#color_array = [get_color_and_label(y_train[i])[0] for i in range(len(X_transformed))]
+#label_array = [get_color_and_label(y_train[i])[1] for i in range(len(X_transformed))]
+#plot.scatter(X_transformed[:, 0], X_transformed[:, 1], c=y_train, label=y_train)
 
-plot.set_xticks(())
-plot.set_yticks(())
+class_no = 10
+for class_ in range(10):
+    indices = np.where(y_train == class_)
+    plot.scatter(X_transformed[indices, 0], X_transformed[indices, 1], label=class_)
+    
+plt.legend(fontsize = 30)
+
+
+plt.xticks(fontsize = 30)
+plt.yticks(fontsize = 30)
+plt.xlabel('PCA axis 1', fontsize = 40)
+plt.ylabel('PCA axis 2', fontsize = 40)
+
 
 plt.tight_layout()
-plt.savefig("mnist_pca1.png")
+plt.savefig("FasionMNISTPCA.png")
